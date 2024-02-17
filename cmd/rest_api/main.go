6b25d0da8517f1602f3272cee402f46e3c17020b
Henrique Gomes Nunes <henriquegomesnunes19@gmail.com>
2024-02-17 13:05:19 -0300

    feat: implement api entrypoint

---
+++ cmd/rest_api/main.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"database/sql"
+	"log"
+	"net/http"
+
+	_ "github.com/lib/pq"
+	"github.com/zhenriquegomes/rinha-backend-2024/internal/infra/repository"
+	"github.com/zhenriquegomes/rinha-backend-2024/internal/infra/rest/routes"
+)
+
+func main() {
+	db, err := sql.Open("postgres", "postgres://postgres:postgres@localhost:5432/postgres?sslmode=disable")
+	if err != nil {
+		log.Fatalf("Não foi possível se conectar do postgres: %v", err)
+	}
+	defer db.Close()
+	clientRepository := repository.NewClientRepository(db)
+	clientRouter := routes.NewClientRouter(clientRepository)
+	http.HandleFunc("POST /clientes/{id}/transacoes", clientRouter.RealizarTransacao)
+	log.Println("Starting server on: http://localhost:8080")
+	log.Fatal(http.ListenAndServe("localhost:8080", nil))
+}
+
