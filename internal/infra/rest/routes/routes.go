24c629b451c795576d72cd495ec1e6653dc6e8dd
Henrique Gomes Nunes <henriquegomesnunes19@gmail.com>
2024-02-19 07:27:58 -0300

    feat: implement new validations on transaction endpoint

--- internal/infra/rest/routes/routes.go
+++ internal/infra/rest/routes/routes.go
@@ -1,6 +1,7 @@
 package routes
 
 import (
+	"database/sql"
 	"encoding/json"
 	"fmt"
 	"net/http"
@@ -36,14 +37,47 @@ func (c *ClientRouter) RealizarTransacao(w http.ResponseWriter, r *http.Request)
 	transaction.ClientID = id
 	dataReferencia := time.Now()
 	transaction.DataReferencia = &dataReferencia
+	client, err := c.repository.ConsultarCliente(id)
+	if err == sql.ErrNoRows {
+		http.Error(w, "Cliente não encontrado", http.StatusNotFound)
+		return
+	} 
+	if err != nil {
+		http.Error(w, fmt.Sprintf("Erro ao consultar o cliente: %v", err), http.StatusInternalServerError)
+		return
+	}
+	err = client.ValidateTransaction(&transaction)
+	if err == entity.ErrEstouroLimite {
+		http.Error(w, err.Error(), http.StatusUnprocessableEntity)
+		return
+	}
+	if err == entity.ErrTipoTransacaoInvalido {
+		http.Error(w, err.Error(), http.StatusUnprocessableEntity)
+		return
+	}
+	if err == entity.ErrDescricaoInvalida {
+		http.Error(w, err.Error(), http.StatusUnprocessableEntity)
+		return
+	}
+	if err != nil {
+		http.Error(w, "Erro ao validar transação", http.StatusInternalServerError)
+		return 
+	}
 	err = c.repository.RealizarTransacao(&transaction)
 	if err != nil {
 		http.Error(w, fmt.Sprintf("Erro ao realizar transação: %v", err), http.StatusInternalServerError)
 		return
 	}
+	novoSaldo := client.SaldoAtual - transaction.Valor
+	err = c.repository.AtualizarSaldo(novoSaldo, id)
+	if err != nil {
+		http.Error(w, fmt.Sprintf("Erro ao atualizar saldo: %v", err), http.StatusInternalServerError)
+		return
+	}
+	client, _ = c.repository.ConsultarCliente(id)
 	transactionResponse := entity.TransactionResponse{
-		Limite: 1000000,
-		Saldo: 900000,
+		Limite: client.Limite,
+		Saldo: client.SaldoAtual,
 	}
 	w.Header().Set("Content-Type", "application/json")
 	err = json.NewEncoder(w).Encode(&transactionResponse)
