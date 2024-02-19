2b7746133049484f37db81bde6aa051482d2f160
Henrique Gomes Nunes <henriquegomesnunes19@gmail.com>
2024-02-19 07:26:50 -0300

    feat: add transaction validation

--- internal/entity/entity.go
+++ internal/entity/entity.go
@@ -1,6 +1,16 @@
 package entity
 
-import "time"
+import (
+	"errors"
+	"time"
+)
+
+
+var (
+	ErrEstouroLimite = errors.New("a transação estourou o limite do cliente")
+	ErrTipoTransacaoInvalido = errors.New("tipo de transação inválido")
+	ErrDescricaoInvalida = errors.New("descrição de transação inválida")
+)
 
 type Transaction struct {
 	ClientID int
@@ -20,4 +30,25 @@ type Cliente struct {
 	Limite int `json:"limite"`
 	SaldoInicial int `json:"saldo_inicial"`
 	SaldoAtual int `json:"saldo_atual"`
+}
+
+func (c *Cliente) abs(n int) int {
+	if n > 0 {
+		return n 
+	} else {
+		return -n
+	}
+}
+
+func (c *Cliente) ValidateTransaction(transaction *Transaction) error {
+	if transaction.Tipo == "d" && c.abs(c.SaldoAtual - transaction.Valor) > c.Limite {
+		return ErrEstouroLimite
+	}
+	if transaction.Tipo != "c" && transaction.Tipo != "d" {
+		return ErrTipoTransacaoInvalido
+	}
+	if transaction.Descricao == "" || len(transaction.Descricao) > 10 {
+		return ErrDescricaoInvalida
+	}
+	return nil
 }
